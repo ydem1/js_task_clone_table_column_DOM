db94615393969c7e9b36f6220f9c79ee1489a746
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-07 19:53:10 +0200

    make build

---
+++ index.58ba90cb.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var n=document.createElement(e.children[1].localName),t=e.children[4];n.innerText=e.children[1].textContent,t.parentNode.insertBefore(n,t)});
+//# sourceMappingURL=index.58ba90cb.js.map
